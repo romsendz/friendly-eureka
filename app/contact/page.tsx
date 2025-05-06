79c7429666a05b7db3c1d3fa2a768b8a55154216
romsendz <sendzyuk.r@gmail.com>
2025-05-06 12:42:49 +0200

    new routes added

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export default function Page() {
+  return <ul>contact</ul>;
+}
+
+export const metadata: Metadata = {
+  title: "Contacto | Empresa",
+  description: "Empresa Empresa Empresa",
+};
---
+++ app/services/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export default function Page() {
+  return <ul>services</ul>;
+}
+
+export const metadata: Metadata = {
+  title: "Nuestros servicios | Empresa",
+  description: "Empresa Empresa Empresa",
+};
