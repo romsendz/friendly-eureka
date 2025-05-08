3ca4f6526a6fd4553b5f7df256de9e15a34093f7
romsendz <sendzyuk.r@gmail.com>
2025-05-08 13:09:34 +0200

    improved appearance / responsiveness

--- components/contact-button.tsx
+++ components/contact-button.tsx
@@ -12,7 +12,12 @@ const ContactButton = () => {
         "lg:text-md text-xs md:text-sm xl:text-lg",
       )}
     >
-      <Link href="/contact">Contáctenos</Link>
+      <Link
+        className="!px-3 shadow-2xl [box-shadow:rgba(136,_165,_191,_0.3)_6px_2px_16px_0px,_rgba(255,_255,_255,_0.2)_-6px_-2px_16px_0px] md:![padding:auto] xl:!px-5 xl:!py-5"
+        href="/contact"
+      >
+        Contáctenos
+      </Link>
     </Button>
   );
 };
