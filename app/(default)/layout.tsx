4230c2ca519cb04fe667f9d4a3d78ccda72c75ac
romsendz <sendzyuk.r@gmail.com>
2025-05-13 13:33:17 +0200

    display contact section on every page

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,7 +1,16 @@
+import ContactUs from "@/components/section/contact-us";
+import { Separator } from "@/components/ui/separator";
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="container mx-auto px-8 py-16">{children}</div>;
+  return (
+    <>
+      <div className="container mx-auto px-8 py-16">{children}</div>
+      <Separator className="mb-4 border-8" />
+      <ContactUs className="py-12" />
+    </>
+  );
 }
